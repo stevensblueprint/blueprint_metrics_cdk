d04a19e5b6915ea00215f6ad4ab2305960f9dd9f
miguel-merlin <mmerlin@stevens.edu>
2026-02-01 00:14:33 -0500

    Initial commit

---
+++ test/blueprint_metrics_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BlueprintMetricsCdk from '../lib/blueprint_metrics_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/blueprint_metrics_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BlueprintMetricsCdk.BlueprintMetricsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
